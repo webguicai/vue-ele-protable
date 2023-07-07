f9570641efa8abeb02d943668bb7b43bc3a6fb72
jyc <jiangyc@asiainfo.com>
2023-07-07 16:24:57 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,15 @@
 <template>
   <div id="app">
-    <ProTable msg="Welcome to Your Vue.js App"/>
+    
   </div>
 </template>
 
 <script>
-import ProTable from './vueProtable/proTable.vue'
 
 export default {
   name: 'App',
-  components: {
-    ProTable
-  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>

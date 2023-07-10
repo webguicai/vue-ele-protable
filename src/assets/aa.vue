01e6d7a173e11ec3e3f7a0f75f9d16a65a139fff
jyc <jiangyc@asiainfo.com>
2023-07-10 10:25:21 +0800

    readme修改

---
+++ src/assets/aa.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <pro-form :formItems="formItems" />
+  </div>
+</template>
+
+<script>
+import ProForm from './ProForm.vue';
+import { ElInput, ElInputNumber } from 'element-plus';
+
+export default {
+  components: {
+    ProForm,
+    ElInput,
+    ElInputNumber,
+  },
+  data() {
+    return {
+      formItems: [
+        {
+          label: '用户名',
+          field: 'username',
+          component: 'ElInput',
+          componentProps: {
+            placeholder: '请输入用户名',
+          },
+        },
+        {
+          label: '年龄',
+          field: 'age',
+          component: 'ElInputNumber',
+          componentProps: {
+            placeholder: '请输入年龄',
+          },
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/elFormCom/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <el-form :model="formData" ref="form" @submit.native.prevent="handleSubmit">
+    <el-form-item v-for="item in formItems" :key="item.field" :label="item.label" :prop="item.field">
+      <el-form-component :component="item.component" :props="item.componentProps"></el-form-component>
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" native-type="submit">提交</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    formItems: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      formData: {},
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.$refs.form.validate((valid) => {
+        if (valid) {
+          // 处理表单提交
+          console.log(this.formData);
+        }
+      });
+    },
+  },
+});
+</script>
